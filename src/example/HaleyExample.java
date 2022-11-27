43bc3a35dcf035dd431baae4a4373fad47b5e19d
Haley Brightwell <hbrightwell1101@gmail.com>
2022-11-26 18:13:07 -0600

    Add all files

---
+++ src/example/HaleyExample.java
@@ -0,0 +1,37 @@
+package example;
+
+import java.util.Scanner;
+
+public class HaleyExample {
+	
+	public static void main(String[] args) {
+		
+		Scanner objScanner = new Scanner(System.in);
+		
+		//objScanner.nextLine();
+		
+		String str = "Hello";
+		
+		Integer num = 5;
+		
+		Boolean x = true;
+		
+		Float y = (float) 1.1;
+		
+		String input = objScanner.nextLine();
+		
+		System.out.println(input);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
+
